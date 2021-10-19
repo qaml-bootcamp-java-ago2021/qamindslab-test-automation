9e53117249f6511b477a6a8db932b72f937c1a22
ziriako <ziriako@gmail.com>
2021-10-18 22:38:14 -0500

    Loop

--- src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
@@ -1,4 +1,24 @@
 package com.qamindslab.moduleone.java.exercise.loops;
 
 public class JavaLoop {
+    public static void main(String args[]) {
+
+        for (int i = 1; i <= 15; i++) {
+            System.out.println(i);
+
+            String respuesta="";
+
+            if (i%3 == 0) {
+                respuesta = "Fizz";
+            }
+            if (i%5 == 0) {
+                respuesta = "Buzz";
+            }
+            if (i%3==0 && i%5==0) {
+                respuesta="FizzBuzz";
+            }
+            System.out.println(respuesta);
+
+        }
+    }
 }
