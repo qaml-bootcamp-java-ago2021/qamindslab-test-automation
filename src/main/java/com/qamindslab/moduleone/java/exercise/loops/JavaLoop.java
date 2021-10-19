645fb7d019f4482d3a2ecffd96d76038a58a1029
r0ds0lg <rod.sol.gab@gmail.com>
2021-10-18 22:16:24 -0500

    Eliminating typo (misplaced white space between Fizz Buzz)

--- src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
@@ -6,7 +6,7 @@ public class JavaLoop {
         for (int i = 1; i <= 100; i++)
         {
             if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("Fizz Buzz");
+                System.out.println("FizzBuzz");
             } else if (i % 3 == 0){
                 System.out.println("Fizz");
             } else if (i % 5 == 0 ) {
