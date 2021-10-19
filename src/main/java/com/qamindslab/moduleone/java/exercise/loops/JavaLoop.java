d91a209a71999bbbba26273a85f0902bff31715f
Jesus Serrano <jesus.serrano@muuklabs.com>
2021-10-18 19:50:13 -0700

    Java Loop Exercise

--- src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
@@ -1,4 +1,20 @@
 package com.qamindslab.moduleone.java.exercise.loops;
 
 public class JavaLoop {
+
+    public static void main(String[] args) {
+        for(int i = 1; i < 100; i++){
+           if(i % 3 == 0){
+               if(i % 5 == 0){
+                   System.out.println("FizzBuzz " + i);
+               }else{
+                   System.out.println("Fizz " + i);
+               }
+           }else if(i % 5 == 0){
+               System.out.println("Buzz " + i);
+           }else {
+               System.out.println(i);
+           }
+        }
+   }
 }
