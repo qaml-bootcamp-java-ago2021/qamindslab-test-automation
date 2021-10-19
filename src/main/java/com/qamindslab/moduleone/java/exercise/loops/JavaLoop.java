e804df6feec73b4715d79c159a0fbad2e1eb3b7f
marenate <mare-vc@hotmail.com>
2021-10-18 23:00:58 -0500

    clase1:exercise loop. Number 1:10 multiple 3 shows: Friz, multiple 5 shows: Buzz, multiple both shows FrizBuzz

--- src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/loops/JavaLoop.java
@@ -1,4 +1,29 @@
 package com.qamindslab.moduleone.java.exercise.loops;
 
 public class JavaLoop {
+
+    public static void main (String [] args){
+
+        for (int i=1; i<=100; i++) {
+            if(i%3 ==0){
+                System.out.println("Friz");
+            }
+            if(i%5 ==0){
+                System.out.println("Buzz");
+            }
+            if(i%3 ==0 & i%5==0){
+                System.out.println("FrizBuzz");
+            }
+            if(i%3 !=0 & i%5!=0){
+                System.out.println(i);
+            }
+
+
+        }
+
+
+
+
+    }
+
 }
