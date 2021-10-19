66518d0b95dbd215d10712180d015912544821be
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-18 20:54:30 -0500

    Maven project support and Hello World Class

---
+++ src/main/java/com/qamindslab/moduleone/java/example/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.qamindslab.moduleone.java.example;
+
+public class HelloWorld {
+    public void static void main(String args[]){
+        System.out.println("Hellow World " + args[0]);
+    }
+}
