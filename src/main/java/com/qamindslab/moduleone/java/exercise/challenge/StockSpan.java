e48a1b3700c6afab0bcc2917f960809c85622e09
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-20 18:13:42 -0500

    Implementing Stock Span exercise

--- src/main/java/com/qamindslab/moduleone/java/exercise/challenge/StockSpan.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/challenge/StockSpan.java
@@ -1,4 +1,29 @@
 package com.qamindslab.moduleone.java.exercise.challenge;
 
 public class StockSpan {
+    public static void main(String[] args) {
+        int stockPrices [] = new int[] {100, 60, 70, 65, 80, 85};
+        int stockSpan [] = new int[stockPrices.length];
+
+        stockSpan[0] = 1;
+
+        for(int stockPriceIndex =1; stockPriceIndex<stockPrices.length; stockPriceIndex++){
+            stockSpan[stockPriceIndex] = 1;
+            System.out.println(stockPrices[stockPriceIndex]);
+
+            int stockPricePreviousIndex = stockPriceIndex-1;
+
+            while((stockPricePreviousIndex >= 0) && (stockPrices[stockPriceIndex] >= stockPrices[stockPricePreviousIndex])){
+                stockSpan[stockPriceIndex]++;
+                stockPricePreviousIndex--;
+            }
+/*            for(int j=stockPriceIndex-1; (j >= 0) && (stockPrices[stockPriceIndex] >= stockPrices[j]); j--){
+                stockSpan[stockPriceIndex]++;
+            }*/
+        }
+
+        for(int i : stockSpan){
+            System.out.println(i);
+        }
+    }
 }
