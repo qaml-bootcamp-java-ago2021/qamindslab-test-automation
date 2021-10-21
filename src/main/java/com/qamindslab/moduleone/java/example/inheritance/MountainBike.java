233dde6fff9fdc4aa27971b7bb274d96a6945429
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-20 21:12:18 -0500

    Adding inheritance and interfaces example implementations

--- src/main/java/com/qamindslab/moduleone/java/example/inheritance/MountainBike.java
+++ src/main/java/com/qamindslab/moduleone/java/example/inheritance/MountainBike.java
@@ -1,4 +1,15 @@
 package com.qamindslab.moduleone.java.example.inheritance;
 
-public class MountainBike {
+public class MountainBike extends Bike{
+    private int seatHeight;
+
+    MountainBike (String brand){
+        super(brand);
+        this.seatHeight = 0;
+    }
+
+
+    public void setSeatHeight(int newHeight){
+        this.seatHeight = newHeight;
+    }
 }
