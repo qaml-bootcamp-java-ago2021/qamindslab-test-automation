b082549a0005092f6a02b81108112b45f422b89a
BCA <belen.nightwish@gmail.com>
2021-10-23 20:31:15 -0500

    se suben los ejercicios de multiplos y palindromos

--- src/main/java/com/qamindslab/moduleone/java/exercise/Palindrome.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/Palindrome.java
@@ -1,4 +1,16 @@
 package com.qamindslab.moduleone.java.exercise;
 
 public class Palindrome {
+    public static void main (String args []) {
+        String word ="anitalavalatina";
+        String rever = "";
+        for(int i=word.length() -1;i>=0;i--) {
+            rever += word.charAt(i);
+        }
+        if(word.compareTo(rever)==0) {
+            System.out.println(word + " es palindromo " + rever);
+        } else {
+            System.out.println(word + " No es palindromo");
+        }
+    }
 }
