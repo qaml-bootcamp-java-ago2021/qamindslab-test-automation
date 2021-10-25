bf8b4a1868774c9dd1a5b0a80f8d13ec1ab1dbdb
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-25 18:21:05 -0500

    Implementing interfaces

--- src/main/java/com/qamindslab/moduleone/java/exercise/interfaces/Truck.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/interfaces/Truck.java
@@ -1,4 +1,31 @@
 package com.qamindslab.moduleone.java.exercise.interfaces;
 
-public class Truck {
+import com.qamindslab.moduleone.java.example.interfaces.Tank;
+import com.qamindslab.moduleone.java.example.interfaces.Vehicle;
+
+public class Truck implements Vehicle {
+    int speed;
+
+    public Truck(){
+        this.speed = 0;
+    }
+
+    public static void main(String[] args) {
+        Vehicle truck = new Truck();
+        truck.printSpec();
+
+        Vehicle tank = new Tank();
+        tank.printSpec();
+    }
+
+    @Override
+    public void speedUp() {
+        this.speed += 2;
+        System.out.println(String.format("New speed %s", this.speed));
+    }
+
+    @Override
+    public void printSpec() {
+        System.out.println(this.getClass().getName());
+    }
 }
