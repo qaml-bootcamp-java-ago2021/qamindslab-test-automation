22528d100f489f32eb6b5efa6ee2cc0c55b89862
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-25 17:53:46 -0500

    Implementing Human years

--- src/main/java/com/qamindslab/moduleone/java/exercise/classes/Dog.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/classes/Dog.java
@@ -1,5 +1,11 @@
 package com.qamindslab.moduleone.java.exercise.classes;
 
 public class Dog {
+    public static void main(String[] args) {
+        assert getHumanYears(6) == 43 : "Incorrect calculation for Dog age in human years";
+    }
 
+    public static float getHumanYears(int dogAge){
+        return dogAge*7;
+    }
 }
--- src/main/java/com/qamindslab/moduleone/java/exercise/classes/PhoneBook.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/classes/PhoneBook.java
@@ -1,4 +1,5 @@
 package com.qamindslab.moduleone.java.exercise.classes;
 
 public class PhoneBook {
+
 }
