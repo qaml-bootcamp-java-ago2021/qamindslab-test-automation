c70b7e2a4ba49218660885e1307f986272852441
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-25 17:35:42 -0500

    Implementing assertions exercises.

--- src/main/java/com/qamindslab/moduleone/java/exercise/assertions/Assertion.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/assertions/Assertion.java
@@ -1,4 +1,18 @@
 package com.qamindslab.moduleone.java.exercise.assertions;
 
 public class Assertion {
+    public static void main(String[] args) {
+        assert "Casting".equals("Dummy String") : "Incorrect strings.";
+
+        //Integer numberToCompare = 3;
+        //assert numberToCompare.equals("test") : "An Integer is not equal to a String";
+
+        //assert (6 >= 0) && (6<=5) : "Number is not between 0 and 5";
+
+        //assert subtract(4, 2) == 1 : "Subtraction is not right.";
+    }
+
+    public static int subtract(int a, int b){
+        return a - b;
+    }
 }
