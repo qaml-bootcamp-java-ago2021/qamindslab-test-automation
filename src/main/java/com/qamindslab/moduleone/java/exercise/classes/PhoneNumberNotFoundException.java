48bfa74207563cbda77c899e869fcde8d47262cf
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-25 18:10:03 -0500

    Implementing Phone Book exercise

---
+++ src/main/java/com/qamindslab/moduleone/java/exercise/classes/PhoneNumberNotFoundException.java
@@ -0,0 +1,7 @@
+package com.qamindslab.moduleone.java.exercise.classes;
+
+public class PhoneNumberNotFoundException extends Exception{
+    public PhoneNumberNotFoundException(String errorMessage){
+        super(errorMessage);
+    }
+}
