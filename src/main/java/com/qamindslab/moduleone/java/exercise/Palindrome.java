4cf915c649fd103b1908b9b800b2a33e2e46f5a9
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-25 17:22:29 -0500

    Implementing Palindrome

--- src/main/java/com/qamindslab/moduleone/java/exercise/Palindrome.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/Palindrome.java
@@ -1,4 +1,27 @@
 package com.qamindslab.moduleone.java.exercise;
 
 public class Palindrome {
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.isPalindrome("No lemon, no melon"));
+    }
+
+    public boolean isPalindrome(String stringToEvaluate){
+        String noSpacesString = stringToEvaluate.replace(" ", "");
+        char[] strings = noSpacesString.toCharArray();
+
+        String swappedString = reverseString(strings);
+
+        return swappedString.equalsIgnoreCase(noSpacesString);
+    }
+
+    private String reverseString(char strings []){
+        String reversedString = "";
+
+        for(int i = strings.length-1; i >=0; i--){
+            reversedString += strings[i];
+        }
+
+        return reversedString;
+    }
 }
