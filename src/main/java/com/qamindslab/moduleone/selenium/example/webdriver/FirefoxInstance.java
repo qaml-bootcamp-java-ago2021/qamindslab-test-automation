8255b9a5047c8e9aa996edb6cdbe7e56a7dc5feb
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-25 21:19:11 -0500

    Creating a FirefoxDriver instance and ChromeDriver instance.

---
+++ src/main/java/com/qamindslab/moduleone/selenium/example/webdriver/FirefoxInstance.java
@@ -0,0 +1,18 @@
+package com.qamindslab.moduleone.selenium.example.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+
+public class FirefoxInstance {
+    public static void main(String[] args) {
+        File rootPath = new File("src/main/resources");
+        File firefoxFilePath = new File(rootPath,"geckodriver");
+
+        System.setProperty("webdriver.gecko.driver", firefoxFilePath.getPath());
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com");
+    }
+}
