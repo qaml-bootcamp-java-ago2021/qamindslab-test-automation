c48c8551bd0b86581862557c8b880c53d35b706b
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-26 05:27:05 -0500

    Implementing Exercise 1 and 2.

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/LoadGooglePage.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/LoadGooglePage.java
@@ -1,4 +1,29 @@
 package com.qamindslab.moduleone.selenium.exercise.webdriver.commonmethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoadGooglePage {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        Thread.sleep(1000);
+
+        // Printing page title
+        String pageTitle = driver.getTitle();
+        System.out.println("PAGE TITLE: " + pageTitle);
+
+        // Printing current url
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("CURRENT URL: " + currentUrl);
+
+        // Printing source code
+        String pageSource = driver.getPageSource();
+        System.out.println("PAGE SOURCE: " + pageSource);
+
+        driver.close();
+    }
 }
