a42f907e59c043884bb51fe1f3588f72f336377a
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-27 22:57:08 -0500

    Implementing the Selenium Exercise 4 - Search in YouTube.

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webelements/LoadYouTube.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webelements/LoadYouTube.java
@@ -1,4 +1,44 @@
 package com.qamindslab.moduleone.selenium.exercise.webelements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class LoadYouTube {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        // Create an instance of ChromeDriver.
+        WebDriver driver = new ChromeDriver();
+
+        // Waiting for a page to be loaded.
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        // Open the Website to interact with.
+        driver.get("https://www.youtube.com/");
+
+        // Find search bar element through XPATH= //input[@id='search']
+        WebElement searchBar = driver.findElement(By.xpath("//input[@id='search']"));
+
+        // Check if element is displayed and enabled
+        if(searchBar.isDisplayed() && searchBar.isEnabled()){
+
+            // Type Selenium
+            searchBar.sendKeys("Selenium");
+
+            // Find search button element: ID = search-icon-legacy
+            WebElement searchButton = driver.findElement(By.id("search-icon-legacy"));
+            // Click button
+            searchButton.click();
+        }
+
+        // Wait 5 seconds
+        Thread.sleep(5000);
+
+        // Close current window.
+        driver.close();
+    }
 }
