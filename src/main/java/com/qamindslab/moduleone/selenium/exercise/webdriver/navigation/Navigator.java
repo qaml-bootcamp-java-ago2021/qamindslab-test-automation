11f3fd50e5f6e18efe0aa086f81bfa67d5b10caa
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-27 22:53:29 -0500

    Implementing Selenium Exercise 3

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/navigation/Navigator.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/navigation/Navigator.java
@@ -1,4 +1,31 @@
 package com.qamindslab.moduleone.selenium.exercise.webdriver.navigation;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Navigator {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.get("https://www.facebook.com");
+
+        driver.get("https://www.espn.com");
+
+        driver.navigate().back();
+        driver.navigate().back();
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().forward();
+        System.out.println(driver.getCurrentUrl());
+
+        assert driver.getCurrentUrl().equals("https://www.facebook.com/") : "Url is incorrect.";
+
+        driver.navigate().refresh();
+        System.out.println(driver.getCurrentUrl());
+
+        driver.quit();
+    }
 }
