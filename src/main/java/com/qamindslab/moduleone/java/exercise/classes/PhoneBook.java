c47a6f69c1123c08c6c1a4a9cb204999d1d6202a
BCA <belen.nightwish@gmail.com>
2021-10-27 19:40:24 -0500

    se agregan cambios

--- src/main/java/com/qamindslab/moduleone/java/exercise/classes/PhoneBook.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/classes/PhoneBook.java
@@ -1,9 +1,30 @@
 package com.qamindslab.moduleone.java.exercise.classes;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
 
 public class PhoneBook {
 
     public static void main (String args []) {
+        List<PhoneBook> nombre = new LinkedList<PhoneBook>();
+        List<PhoneBook> telefono = new LinkedList<PhoneBook>();
+        long antes;
 
 
     }
+
+
+
+    public void add(List nombre, List telefono) {
+        nombre.add("Belen");
+        telefono.add("9711070620");
+        nombre.add("Daniel");
+        telefono.add("5511070620");
+        nombre.add("Alejandra");
+        telefono.add("5543890998");
+    }
+
+
 }
