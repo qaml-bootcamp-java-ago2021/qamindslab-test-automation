8a8fb056f2d10c2aa1ffe553339ab609cec7b062
marenate <mare-vc@hotmail.com>
2021-10-28 22:25:53 -0500

    LoadGooglePage

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/LoadGooglePage.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/LoadGooglePage.java
@@ -1,4 +1,18 @@
 package com.qamindslab.moduleone.selenium.exercise.webdriver.commonmethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoadGooglePage {
+
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        System.out.println("Titulo: "+driver.getTitle());
+        System.out.println("URL: "+driver.getCurrentUrl());
+        System.out.println("Soruce: "+driver.getPageSource());
+        driver.close();
+    }
 }
