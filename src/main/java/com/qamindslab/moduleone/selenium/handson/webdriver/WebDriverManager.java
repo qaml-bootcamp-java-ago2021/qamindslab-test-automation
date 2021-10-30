a75e04d17d1d018683e305978193660efe1b10b1
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-30 10:41:04 -0500

    Implementing WebDriverManager

--- src/main/java/com/qamindslab/moduleone/selenium/handson/webdriver/WebDriverManager.java
+++ src/main/java/com/qamindslab/moduleone/selenium/handson/webdriver/WebDriverManager.java
@@ -1,5 +1,30 @@
 package com.qamindslab.moduleone.selenium.handson.webdriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.util.Locale;
+
 // Hands On #1 - GetCorrectWebDriver
 public class WebDriverManager {
+
+    public static WebDriver getDriver(String browser){
+        File rootPath = new File("src/main/resources");
+
+        if(browser.toLowerCase(Locale.ROOT).equals("chrome")){
+            File chromeFilePath = new File(rootPath, "chromedriver");
+
+            System.setProperty("webdriver.chrome.driver", chromeFilePath.getPath());
+            return new ChromeDriver();
+        }else if(browser.toLowerCase(Locale.ROOT).equals("firefox")){
+            File firefoxFilePath = new File(rootPath, "geckodriver");
+
+            System.setProperty("webdriver.gecko.driver", firefoxFilePath.getPath());
+            return new FirefoxDriver();
+        } else {
+            return null;
+        }
+    }
 }
