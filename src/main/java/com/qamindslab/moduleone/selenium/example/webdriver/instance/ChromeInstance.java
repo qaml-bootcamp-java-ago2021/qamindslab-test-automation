dd032cc49569187512b3d57521e077ef868e17ac
Marco De Jesus <marcodejesus2010@gmail.com>
2021-10-30 11:14:35 -0500

    Enhancing WebDriverManager Factory

--- src/main/java/com/qamindslab/moduleone/selenium/example/webdriver/instance/ChromeInstance.java
+++ src/main/java/com/qamindslab/moduleone/selenium/example/webdriver/instance/ChromeInstance.java
@@ -1,15 +1,17 @@
 package com.qamindslab.moduleone.selenium.example.webdriver.instance;
 
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withif.Browser;
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withif.NotWebDriverImplementedException;
 import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withif.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeInstance {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotWebDriverImplementedException {
        // System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
       //  WebDriver driver = new ChromeDriver();
 
-        WebDriver driver = WebDriverManager.getDriver("CHROME");
+        WebDriver driver = WebDriverManager.getDriver(Browser.CHROME);
 
         driver.get("https://www.google.com");
     }
