6d7a9c4e6a3bd65a29f4d19ba30c3c66491b0381
Jesus Serrano <jesus.serrano@muuklabs.com>
2021-10-31 13:10:44 -0700

    LoadGooglePage

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/LoadGooglePage.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/LoadGooglePage.java
@@ -1,4 +1,38 @@
 package com.qamindslab.moduleone.selenium.exercise.webdriver.commonmethods;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoadGooglePage {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+        driver = new FirefoxDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void LoadGooglePage() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        driver.manage().window().setSize(new Dimension(1661, 933));
+
+        Thread.sleep(5000);
+        driver.close();
+    }
 }
