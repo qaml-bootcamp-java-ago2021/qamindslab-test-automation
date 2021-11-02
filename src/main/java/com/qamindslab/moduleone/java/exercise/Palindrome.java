74d5646ea9fbb539e7e8af38f0dedc775338dad6
Gaz Letona <gagarlopz@gmail.com>
2021-11-01 20:43:28 -0600

    Se agrega Palindromo

--- src/main/java/com/qamindslab/moduleone/java/exercise/Palindrome.java
+++ src/main/java/com/qamindslab/moduleone/java/exercise/Palindrome.java
@@ -1,4 +1,25 @@
 package com.qamindslab.moduleone.java.exercise;
 
 public class Palindrome {
+
+    public static void main(String[] args) {
+        int inicio = 0;
+        String frase = "amolapacificapaloma";
+        int finalFrase = frase.length()-1;
+        boolean esPalindromo = false;
+
+        while ((inicio < finalFrase) && (esPalindromo == false)) {
+            if (frase.charAt(inicio) == frase.charAt(finalFrase)) {
+                inicio++;
+                finalFrase--;
+            } else {
+                esPalindromo = true;
+            }
+        }
+        if (esPalindromo == false) {
+            System.out.println("Es palindromo");
+        } else {
+            System.out.println("No es palindromo");
+        }
+    }
 }
