e5b5a98871193c43c6522f4ae8fe1b02dc1434d8
BCA <belen.nightwish@gmail.com>
2021-11-01 23:37:47 -0600

    se agrega ejercicio 2

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/navigation/Navigator.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/navigation/Navigator.java
@@ -1,4 +1,17 @@
 package com.qamindslab.moduleone.selenium.exercise.webdriver.navigation;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Navigator {
+
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println("Titulo: "+driver.getTitle());
+        System.out.println("URL: "+driver.getCurrentUrl());
+        System.out.println("Soruce: "+driver.getPageSource());
+        driver.close();
+    }
+
 }
