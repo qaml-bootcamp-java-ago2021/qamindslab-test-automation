37c12fa2d6d6154e4e3cab895011fdff213a9944
BCA <belen.nightwish@gmail.com>
2021-11-02 00:30:09 -0600

    se agregn los ejercicios uno y dos del modulo dos

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/MercadoLibreFinder.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/MercadoLibreFinder.java
@@ -1,4 +1,31 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MercadoLibreFinder {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.mercadolibre.com.mx/");
+
+        WebElement busquedaTestBox = driver.findElement(By.name("as_word"));
+        busquedaTestBox.sendKeys("PSP 4");
+
+        WebElement button= driver.findElement(By.className("nav-search-btn"));
+        button.click();
+
+        WebElement price= driver.findElement(By.className("price-tag-fraction"));
+        price.getText();
+
+        WebElement link= driver.findElement(By.className("ui-search-item__title"));
+        link.click();
+
+        Thread.sleep(5000);
+
+    }
+
 }
--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/WaitForGoogle.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/WaitForGoogle.java
@@ -1,4 +1,20 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class WaitForGoogle {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.mx/");
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        driver.findElements(By.id(""));
+
+
+    }
 }
