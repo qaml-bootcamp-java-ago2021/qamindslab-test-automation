ea438c000190d6a2f5984559b3992855d255b061
marenate <mare-vc@hotmail.com>
2021-11-03 17:55:09 -0500

    Tags function, script, id and class quantities frecuency

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/challenge/TagsFrequencyCount.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webdriver/commonmethods/challenge/TagsFrequencyCount.java
@@ -1,4 +1,23 @@
 package com.qamindslab.moduleone.selenium.exercise.webdriver.commonmethods.challenge;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TagsFrequencyCount {
+    public static void main(String [] argas) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(1000);
+        String pageSource = driver.getPageSource();
+        String[] functionWord =  pageSource.split("function");
+        String[] scriptWord = pageSource.split("script");
+        String[] classWord = pageSource.split("class");
+        String[] idWord = pageSource.split("id");
+        System.out.println("Function quantities is: "+functionWord.length);
+        System.out.println("Script word quantities is: "+scriptWord.length);
+        System.out.println("Class word quantities is: "+classWord.length);
+        System.out.println("Id word quantities is: "+idWord.length);
+        driver.close();
+    }
 }
