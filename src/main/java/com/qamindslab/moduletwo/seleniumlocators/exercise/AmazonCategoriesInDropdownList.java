729652266b02759fcc9bd9fbf0afb9febf2ea975
BCA <belen.nightwish@gmail.com>
2021-11-03 23:32:06 -0600

    se agregan ejercicio de clase y la tarea de amazon

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/AmazonCategoriesInDropdownList.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/AmazonCategoriesInDropdownList.java
@@ -1,4 +1,40 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.util.List;
+
 public class AmazonCategoriesInDropdownList {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com.mx/");
+
+        WebElement elemnt = driver.findElement(By.id("searchDropdownBox"));
+        Select dropdown = new Select (elemnt);
+        System.out.println("ID "+dropdown.getAllSelectedOptions());
+
+
+        List<WebElement> opcions = dropdown.getAllSelectedOptions();
+        for (WebElement product : opcions){
+            System.out.println("all opcions"+product.getText());
+        }
+
+
+
+        if (dropdown.isMultiple()==true){
+            System.out.println("multiplo"+elemnt.getText());
+        }
+
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByIndex(4);
+        dropdown.selectByValue("Auto");
+        driver.quit();
+
+    }
+
 }
--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/PortadaInWikipediaPartialLinks.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/PortadaInWikipediaPartialLinks.java
@@ -1,4 +1,37 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class PortadaInWikipediaPartialLinks {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://es.wikipedia.org/wiki/Portada");
+
+        WebElement busquedaTest = driver.findElement(By.linkText("portada"));
+        System.out.println(busquedaTest.getText());
+        System.out.println(busquedaTest.getAttribute("href"));
+
+
+        List<WebElement>  links = driver.findElements(By.partialLinkText("Página"));
+        for (WebElement product : links){
+            System.out.println("paginas: "+product.getText());
+            System.out.println(product.getAttribute("href"));
+        }
+        List<WebElement>  button = driver.findElements(By.tagName("Button"));
+        for (WebElement product : button){
+            System.out.println("button: "+product.getAttribute("title"));
+        }
+
+        driver.quit();
+
+    }
+
 }
