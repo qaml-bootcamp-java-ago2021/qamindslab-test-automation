c42a717caa838d909c9ef63d74a755390705a4d3
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-03 20:51:59 -0600

    Implementing the demonstration for implicitly wait with google

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/WaitForGoogle.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/WaitForGoogle.java
@@ -1,4 +1,24 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.ChromeBrowserDriver;
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
 public class WaitForGoogle {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverManager.getBrowserInstance(new ChromeBrowserDriver());
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+
+        driver.get("https://www.google.com.mx/");
+
+        WebElement searchTextBox = driver.findElement(By.id("ThisIdIsInvalid"));
+
+        driver.close();
+    }
 }
