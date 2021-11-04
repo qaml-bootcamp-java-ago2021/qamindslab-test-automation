47e0863f4e0f84d81ab8ed2b0d95f2acfa360d75
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-03 21:37:02 -0600

    Implementing Amazon categories in dropdonlist

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/AmazonCategoriesInDropdownList.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/AmazonCategoriesInDropdownList.java
@@ -1,4 +1,42 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.ChromeBrowserDriver;
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
+
 public class AmazonCategoriesInDropdownList {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverManager.getBrowserInstance(new ChromeBrowserDriver());
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com.mx/");
+
+        WebElement elementDropdown = driver.findElement(By.id("searchDropdownBox"));
+        Select searchDropdown = new Select(elementDropdown);
+
+        for (WebElement option: searchDropdown.getOptions()) {
+            System.out.println(option.getText());
+        }
+
+        System.out.println(searchDropdown.isMultiple() ? "It's Multiple Dropdown." : "It's not Multiple Dropdown.");
+
+        System.out.println("The first selected option is: " + searchDropdown.getFirstSelectedOption().getText());
+
+        elementDropdown.click();
+        searchDropdown.selectByIndex(0);
+
+        elementDropdown.click();
+        searchDropdown.selectByValue("search-alias=automotive");
+
+        for(WebElement selectedOption : searchDropdown.getAllSelectedOptions()){
+            System.out.println("SELECTED OPTIONS: " + selectedOption.getText());
+        }
+
+        driver.quit();
+    }
 }
