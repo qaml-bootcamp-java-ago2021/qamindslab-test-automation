cf1816b025de3c202589f568fba5fd4e8e9c4657
BCA <belen.nightwish@gmail.com>
2021-11-03 23:56:22 -0600

    se agregan tarea Nasa

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/homework/NASAHyperlinksFinder.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/homework/NASAHyperlinksFinder.java
@@ -1,4 +1,34 @@
 package com.qamindslab.moduletwo.seleniumlocators.homework;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class NASAHyperlinksFinder {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.nasa.gov/");
+
+        WebElement busquedaTest = driver.findElement(By.linkText("Missions"));
+        System.out.println("Text "+busquedaTest.getText());
+        System.out.println("href "+busquedaTest.getAttribute("href"));
+
+        WebElement nassa = driver.findElement(By.linkText("NASA TV"));
+        System.out.println("Text "+nassa.getText());
+        System.out.println("href "+nassa.getAttribute("href"));
+
+        List<WebElement>  names = driver.findElements(By.tagName("input"));
+        for (WebElement product : names){
+            System.out.println("input: "+product.getAttribute("value"));
+        }
+
+        driver.quit();
+
+    }
+
 }
