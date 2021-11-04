c2af4d55e74565905ec24be3708df4d9852bd777
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-03 20:37:46 -0600

    Updating locators strategies with tagName, linktext and partialLinkText methods.

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/example/locators/LocatorsLinkTag.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/example/locators/LocatorsLinkTag.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class LocatorsLinkTag {
 
     public static void main(String[] args) throws InterruptedException {
@@ -29,10 +31,18 @@ public class LocatorsLinkTag {
         Thread.sleep(5000);
 
         // Finding the 'PRODUCTS' title element through its tag name: 'span'
+        WebElement productTitle = driver.findElement(By.tagName("span"));
+        System.out.println(productTitle.getText());
 
         // Finding link elements that has a link text as 'Sauce Labs Backpack'
+        WebElement backpack = driver.findElement(By.linkText("Sauce Labs Backpack"));
+        System.out.println(backpack.getText());
 
         // Finding all link elements that contains a text 'Sauce Labs'.
+        List<WebElement> productList = driver.findElements(By.partialLinkText("Sauce Labs"));
+        for (WebElement product : productList){
+            System.out.println(product.getText());
+        }
 
         driver.quit();
     }
