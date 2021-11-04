2b0c57c3bf86fc85e1290113f9b80d7afb65bb88
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-03 21:30:59 -0600

    Implementing the NASA Homework

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/homework/NASAHyperlinksFinder.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/homework/NASAHyperlinksFinder.java
@@ -1,4 +1,43 @@
 package com.qamindslab.moduletwo.seleniumlocators.homework;
 
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.ChromeBrowserDriver;
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class NASAHyperlinksFinder {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverManager.getBrowserInstance(new ChromeBrowserDriver());
+        driver.manage().window().maximize();
+        driver.get("https://www.nasa.gov/");
+
+        System.out.println("HYPERLINKS WITH TEXT: MISSION");
+        List<WebElement> missionLinks = driver.findElements(By.xpath("//a[contains(text(), 'Mission')]"));
+        for (WebElement mission: missionLinks ) {
+            System.out.println(mission.getAttribute("href"));
+        }
+
+        System.out.println("HYPERLINKS WITH TEXT: NASA TV");
+        List<WebElement> nasaTVLinks = driver.findElements(By.xpath("//a[contains(text(), 'NASA TV')]"));
+        for (WebElement nasaTVLink: nasaTVLinks ) {
+            System.out.println(nasaTVLink.getAttribute("href"));
+        }
+
+        System.out.println("HYPERLINKS WITH TEXT: Mars");
+        List<WebElement> marsLinks = driver.findElements(By.xpath("//a[contains(text(), 'Mars')]"));
+        for (WebElement marsLink: marsLinks ) {
+            System.out.println(marsLink.getAttribute("href"));
+        }
+
+        System.out.println("ELEMENTS OF WITH INPUT TAG NAME.");
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        for (WebElement input: inputs ) {
+            System.out.println(input.getAttribute("value"));
+        }
+
+        driver.quit();
+    }
 }
