f1f18e63f8f5b010b11e308ef5e150b710f83923
marenate <mare-vc@hotmail.com>
2021-11-06 10:08:23 -0500

    Mercado libre - Nasa

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/MercadoLibreFinder.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/MercadoLibreFinder.java
@@ -1,4 +1,25 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MercadoLibreFinder {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get(" https://www.mercadolibre.com.mx/");
+        WebElement textBoxSearch = driver.findElement(By.name("as_word"));
+        textBoxSearch.sendKeys("PS4");
+
+        WebElement btnSearch = driver.findElement(By.className("nav-icon-search"));
+        btnSearch.click();
+        Thread.sleep(2000);
+        WebElement resultSearch = driver.findElement(By.className("ui-search-price__second-line"));
+        resultSearch.getText();
+        Thread.sleep(2000);
+        System.out.println("Price: "+resultSearch.getText());
+        driver.close();
+    }
 }
--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/homework/NASAHyperlinksFinder.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/homework/NASAHyperlinksFinder.java
@@ -1,4 +1,25 @@
 package com.qamindslab.moduletwo.seleniumlocators.homework;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NASAHyperlinksFinder {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.nasa.gov/");
+        WebElement linkTexMission = driver.findElement(By.linkText("Missions"));
+
+        String hrefAttribute = linkTexMission.getAttribute("href");
+        System.out.println("Missins attribure"+hrefAttribute);
+        WebElement linkTexNasaTv = driver.findElement(By.linkText("NASA TV"));
+
+        String hrefAttributeNasaTv = linkTexMission.getAttribute("href");
+        System.out.println("NASA TV attribure"+hrefAttributeNasaTv);
+
+        driver.close();
+    }
+
 }
