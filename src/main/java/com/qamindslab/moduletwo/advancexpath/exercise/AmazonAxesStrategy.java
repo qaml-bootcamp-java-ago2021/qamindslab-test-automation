ff26f174b73e00294ca6e1e781d0e83b957b3ad3
marenate <mare-vc@hotmail.com>
2021-11-06 12:33:29 -0500

    exercise AmazonAxesStrategy

--- src/main/java/com/qamindslab/moduletwo/advancexpath/exercise/AmazonAxesStrategy.java
+++ src/main/java/com/qamindslab/moduletwo/advancexpath/exercise/AmazonAxesStrategy.java
@@ -1,4 +1,26 @@
 package com.qamindslab.moduletwo.advancexpath.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AmazonAxesStrategy {
+    public static void main (String[] args){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com.mx/");
+        WebElement firstDiv = driver.findElement(By.xpath("//body/div[1]"));
+        System.out.println("First Div"+ firstDiv.getText());
+        WebElement lastDiv = driver.findElement(By.xpath("//body/div[last()]"));
+        System.out.println("Last Div"+ lastDiv.getText());
+        WebElement roleNavigation = driver.findElement(By.xpath("//*[@role='navigation']"));
+        System.out.println("attribute role with text navigation: "+ roleNavigation.getText());
+        WebElement classContainNav = driver.findElement(By.xpath("//*[contains(@class,'nav')]"));
+        System.out.println("class attribute Contains Nav: "+ classContainNav .getText());
+        WebElement attributeSubmitClassAttNavInput = driver.findElement(By.xpath("//*[@type='submit'][contains(@class, 'nav-input')]"));
+        System.out.println("submit attribute Contains Nav-input "+ classContainNav .getText());
+        driver.close();
+
+    }
 }
