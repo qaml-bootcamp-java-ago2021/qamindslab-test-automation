bd696a842abd813d44dc15fa4320a6d47054954e
ziriako <ziriako@gmail.com>
2021-11-07 01:18:44 -0600

    Adding LoadYoutube

--- src/main/java/com/qamindslab/moduleone/selenium/exercise/webelements/LoadYouTube.java
+++ src/main/java/com/qamindslab/moduleone/selenium/exercise/webelements/LoadYouTube.java
@@ -1,6 +1,8 @@
 package com.qamindslab.moduleone.selenium.exercise.webelements;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoadYouTube {
@@ -8,6 +10,17 @@ public class LoadYouTube {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.youtube.com/");
+
+        WebElement elementoSearch = driver.findElement(By.id("//*[@id=\"search\"]"));
+
+        elementoSearch.isDisplayed();
+        elementoSearch.isEnabled();
+
+        elementoSearch.sendKeys("Selenium");
+
+        WebElement elementoBoton = driver.findElement(By.id("//*[@id=\"search-icon-legacy\"]"));
+
         //driver.qui();
     }
 }
+
