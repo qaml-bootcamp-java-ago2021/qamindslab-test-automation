b9997ff406313ad3feb767e0a49a7a423ef6bfe2
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-10 16:03:44 -0600

    Adding Google Search files

---
+++ src/test/java/com/qamindslab/modulethree/pom/example/googlesearch/srctest/test/GoogleSearchTest.java
@@ -0,0 +1,6 @@
+package com.qamindslab.modulethree.pom.example.googlesearch.srctest.test;
+
+import com.qamindslab.modulethree.pom.example.googlesearch.srctest.common.BaseTest;
+
+public class GoogleSearchTest extends BaseTest {
+}
