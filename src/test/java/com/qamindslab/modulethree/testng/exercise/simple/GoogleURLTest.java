df39db932323bf5c3481b7859b78873afb325032
marenate <mare-vc@hotmail.com>
2021-11-10 22:08:15 -0500

    pom and  simple test

--- src/test/java/com/qamindslab/modulethree/testng/exercise/simple/GoogleURLTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/simple/GoogleURLTest.java
@@ -1,4 +1,20 @@
 package com.qamindslab.modulethree.testng.exercise.simple;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 public class GoogleURLTest {
+
+        @Test
+         public void googleURL(){
+            String theURl = "https://www.google.com/";
+            System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.google.com");
+            String currentURL = driver.getCurrentUrl();
+            Assert.assertEquals(theURl,currentURL);
+            driver.close();
+    }
 }
