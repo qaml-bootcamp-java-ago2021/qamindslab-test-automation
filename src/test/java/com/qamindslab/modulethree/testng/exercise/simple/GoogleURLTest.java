49923ff863034d4aa404436c17266d4916bfc0ae
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-10 21:14:50 -0600

    Implementing Google URL Test and running through TestNG xml.

--- src/test/java/com/qamindslab/modulethree/testng/exercise/simple/GoogleURLTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/simple/GoogleURLTest.java
@@ -1,4 +1,25 @@
 package com.qamindslab.modulethree.testng.exercise.simple;
 
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.ChromeBrowserDriver;
+import com.qamindslab.moduleone.selenium.example.webdriver.instance.manager.withinterfaces.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 public class GoogleURLTest {
+    @Test
+    public void validateGoogleUrl(){
+        //Arrange
+        String expected = "https://www.google.com/";
+
+        WebDriver driver = WebDriverManager.getBrowserInstance(new ChromeBrowserDriver());
+        driver.get("https://www.google.com/");
+
+        //Act
+        String actual = driver.getCurrentUrl();
+
+        //Assert
+        assertEquals(actual, expected, "Url is not correct.");
+    }
 }
