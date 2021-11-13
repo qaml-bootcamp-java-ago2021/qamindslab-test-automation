f96f526e2af84887cf0bb07656aa3bdc340345fc
Marco De Jesus <marcodejesus2010@gmail.com>
2021-11-13 10:53:41 -0600

    Implementing Selenium WebDriver with TestNG example.

--- src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/GoogleURLTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/GoogleURLTest.java
@@ -1,4 +1,24 @@
 package com.qamindslab.modulethree.testng.exercise.withwebdriver;
 
-public class GoogleURLTest {
+import com.qamindslab.modulethree.testng.example.withwebdriver.common.BaseTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GoogleURLTest extends BaseTest {
+    @Test(description = "Open google page", alwaysRun = true, groups= {"regression"})
+    @Parameters({"baseUrl"})
+    public void validateGoogleUrl(@Optional("") String baseUrl){
+        //Arrange
+        String expected = baseUrl;
+        driver.get("https://www.google.com/");
+
+        //Act
+        String actual = driver.getCurrentUrl();
+
+        //Assert
+        assertEquals(actual, expected, "Url is not correct.");
+    }
 }
