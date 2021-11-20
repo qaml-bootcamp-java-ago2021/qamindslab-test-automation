1336e7c0b5079e91fa0dad357214088040e0e2b1
marenate <mare-vc@hotmail.com>
2021-11-20 09:08:49 -0500

    basetest, googleurltest, xml

--- src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/GoogleURLTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/GoogleURLTest.java
@@ -1,4 +1,23 @@
 package com.qamindslab.modulethree.testng.exercise.withwebdriver;
 
-public class GoogleURLTest {
+import com.qamindslab.modulethree.testng.example.withwebdriver.common.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GoogleURLTest extends BaseTest {
+    @Test(description = "Open google page", alwaysRun = true, groups= {"regression"})
+    @Parameters({"baseUrl"})
+    public void openGoogle(@Optional("https://www.google.com/") String baseUrl){
+        //String theURl = "https://www.google.com/mx";
+        driver.get("https://www.google.com/");
+        assertEquals(driver.getCurrentUrl(), baseUrl, "Didn't get expected google url");
+        driver.close();
+    }
+
 }
--- src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/common/BaseTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/common/BaseTest.java
@@ -1,4 +1,43 @@
 package com.qamindslab.modulethree.testng.exercise.withwebdriver.common;
 
+import com.qamindslab.modulethree.testng.example.withwebdriver.common.exceptions.NotWebDriverImplementedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Optional;
+
 public class BaseTest {
+    public WebDriver driver;
+
+    public void setUp(@Optional("chrome") String browser) throws NotWebDriverImplementedException {
+        driver =  getBrowser(browser);
+        //driver.get("https://www.google.com/");
+
+    }
+
+    public static WebDriver getBrowser(String browser) throws NotWebDriverImplementedException {
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+            WebDriver driver = new ChromeDriver();
+            return driver;
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxProfile profile = new FirefoxProfile();
+            profile.setPreference("permissions.default.desktop-notification", 1);
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+            return new FirefoxDriver(capabilities);
+        }else{
+            throw new NotWebDriverImplementedException("The web browser driver you're looking for is not yet implemented: " + browser);
+        }
+    }
+    @AfterSuite
+    public void tearDown(){
+        driver.quit();
+    }
 }
