1d3051a8bd1400f4d6eb7ca19e18bb1b901dad9f
Jesus Serrano <jesus.serrano@muuklabs.com>
2021-11-20 10:07:54 -0800

    Nov20

--- src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/GoogleURLTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/withwebdriver/GoogleURLTest.java
@@ -1,4 +1,18 @@
 package com.qamindslab.modulethree.testng.exercise.withwebdriver;
 
-public class GoogleURLTest {
+import com.qamindslab.modulethree.testng.exercise.withwebdriver.common.BaseTest;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class GoogleURLTest extends BaseTest {
+
+    @Test(description = "Open google page and verify url", alwaysRun = true, groups= {"regression"})
+    @Parameters({"baseUrl"})
+    public void openGoogle(@Optional("") String baseUrl){
+        driver.get("https://www.google.com.mx/");
+        assertEquals(driver.getCurrentUrl(), baseUrl, "Didn't get expected google url");
+    }
 }
