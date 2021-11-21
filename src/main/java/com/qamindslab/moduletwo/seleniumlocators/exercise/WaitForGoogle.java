7962c8de329bd482da7774615fb292f75ffa3297
ziriako <ziriako@gmail.com>
2021-11-21 01:18:03 -0600

    Adding WaitGoogle

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/WaitForGoogle.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/WaitForGoogle.java
@@ -1,4 +1,24 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class WaitForGoogle {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("Unknown ID"));
+
+        //Thread.sleep(10000);
+        //driver.quit();
+    }
 }
