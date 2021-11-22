f011c08b27859517a69e410d494dee8260a7cc43
ziriako <ziriako@gmail.com>
2021-11-21 20:31:32 -0600

    Adding WikipediaPartialLink

--- src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/PortadaInWikipediaPartialLinks.java
+++ src/main/java/com/qamindslab/moduletwo/seleniumlocators/exercise/PortadaInWikipediaPartialLinks.java
@@ -1,4 +1,29 @@
 package com.qamindslab.moduletwo.seleniumlocators.exercise;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class PortadaInWikipediaPartialLinks {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://es.wikipedia.org/wiki/Portada");
+        WebElement linkPortada = driver.findElement(By.linkText("Portada"));
+        System.out.println("href: "+ linkPortada.getAttribute("href"));
+
+        List<WebElement> listaPagina = driver.findElements(By.partialLinkText("Pagina"));
+        for (WebElement lista : listaPagina){
+            System.out.println(lista.getAttribute("href"));
+        }
+        List<WebElement> listaBoton = driver.findElements(By.tagName("button"));
+        for (WebElement lista : listaBoton){
+            System.out.println(lista.getAttribute("title"));
+        }
+    }
 }
