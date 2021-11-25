e00132f7818b179ea11949a2f925f6e4325c4560
BCA <belen.nightwish@gmail.com>
2021-11-24 18:27:04 -0600

    se actualizan archivos mod 3 y se creas solo estructura mod4

--- src/test/java/com/qamindslab/modulethree/testng/exercise/simple/GoogleURLTest.java
+++ src/test/java/com/qamindslab/modulethree/testng/exercise/simple/GoogleURLTest.java
@@ -1,4 +1,23 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
 package com.qamindslab.modulethree.testng.exercise.simple;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class GoogleURLTest {
+
+
+    @Test
+    public void googleurl(){
+        String Google="https://www.google.com.mx/";
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.mx/");
+        String url=driver.getCurrentUrl();
+
+        Assert.assertEquals(url, Google, "Url Incorrecta");
+
+
+}
 }
