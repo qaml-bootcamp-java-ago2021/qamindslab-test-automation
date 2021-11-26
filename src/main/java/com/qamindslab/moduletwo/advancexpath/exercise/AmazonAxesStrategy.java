374cb46b2d5f6f1a6fe9b7c1d2356a0e50c8affc
ziriako <ziriako@gmail.com>
2021-11-25 22:33:45 -0600

    Adding AmazonAxes

--- src/main/java/com/qamindslab/moduletwo/advancexpath/exercise/AmazonAxesStrategy.java
+++ src/main/java/com/qamindslab/moduletwo/advancexpath/exercise/AmazonAxesStrategy.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AmazonAxesStrategy {
@@ -13,11 +15,29 @@ public class AmazonAxesStrategy {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://es.wikipedia.org/wiki/Selenium");
 
-        WebElement myDynamicElement = driver.findElement(By.id("myDynamicElement"));
+        List<WebElement> seleniumLinks = driver.findElements(By.xpath("//a[contains(text(), 'Selenium')]"));
+        List<String> listOfLinks = new ArrayList<>();
+
+        for (WebElement link : seleniumLinks) {
+            listOfLinks.add(link.getAttribute("href"));
+        }
+
+        for (String link : listOfLinks) {
+            driver.navigate().to(link);
+
+
+            System.out.println("Título: " + driver.getTitle().toUpperCase());
+
+            List<WebElement> aElementSiblings = driver.findElements(By.xpath("//body/descendant::a/following-sibling::*"));
+
+            for (WebElement element : aElementSiblings) {
+                System.out.println("Sibling text: " + element.getText());
+            }
+            driver.navigate().back();
+        }
 
     }
 }
